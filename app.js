7d8f34f0f7d52caed9f6802577a66e8def99f48a
Selmantopal97 <setpa97@hotmail.com>
2023-09-01 15:04:52 +0300

    added controller and route files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 
@@ -8,17 +9,9 @@ app.set("view engine", "ejs");
 //Middlewares
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.status(200).render('index',{
-        page_name: 'index'
-    });
-});
+//Routes
 
-app.get('/about', (req, res) => {
-    res.status(200).render('about',{
-        page_name: 'about'
-    });
-});
+app.use('/', pageRoute);
 
 const port = 3000;
 
