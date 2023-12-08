eebf2348828b12494634cd305e0787281444c0df
Selmantopal97 <setpa97@hotmail.com>
2023-12-08 16:30:28 +0300

    creating category and removing

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,5 +5,7 @@ const categoryController = require('../controllers/categoryController');
 const router = express.Router();
 
 router.post('/', categoryController.createCategory); // POST http://localhost:3000/categories
+router.delete('/:id', categoryController.deleteCategory);
+
 
 module.exports= router;
